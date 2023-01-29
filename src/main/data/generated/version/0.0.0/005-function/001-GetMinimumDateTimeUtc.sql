cb4fede99e6d22869555fcfbc9860fd6c74d0ea0
Doug Wilson <doug@revealix.com>
2023-01-29 15:23:58 -0600

    AAF-26 Fifth Commit
    AAF-33 Create GetMinimumDateTimeUtc and GetMaximumDateTimeUtc function scripts

---
+++ src/main/data/generated/version/0.0.0/005-function/001-GetMinimumDateTimeUtc.sql
@@ -0,0 +1,16 @@
+-- FUNCTION: GetMinimumDateTimeUtc
+
+-- DROP FUNCTION GetMinimumDateTimeUtc ;
+
+CREATE FUNCTION GetMinimumDateTimeUtc()
+        RETURNS timestamp without time zone
+        LANGUAGE plpgsql
+    AS
+$$
+DECLARE
+    minimumDateTimeUtc timestamp without time zone;
+BEGIN
+    SELECT '1900-01-01T00:00:00.000Z'::timestamp INTO minimumDateTimeUtc;
+    RETURN minimumDateTimeUtc;
+END;
+$$
