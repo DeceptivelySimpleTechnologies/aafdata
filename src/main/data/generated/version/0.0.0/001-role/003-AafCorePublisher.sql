ab25bc3703bbdce0d32fd353d6a5e76f62668b96
Doug Wilson <doug@revealix.com>
2023-02-21 12:07:20 -0600

    AAF-26 Eighth Commit
    Row digest calculation and row create (INSERT), read (GET), update (PUT), and delete (DELETE) will be implemented in the EntityDataMicroservice
    
    Create modeler, publisher, administrator, client roles in addition to owner

---
+++ src/main/data/generated/version/0.0.0/001-role/003-AafCorePublisher.sql
@@ -0,0 +1,11 @@
+-- Role: "AafCorePublisher"
+
+-- DROP ROLE "AafCorePublisher";
+
+CREATE ROLE "AafCorePublisher" WITH
+    LOGIN
+    NOSUPERUSER
+    NOINHERIT
+    NOCREATEDB
+    NOCREATEROLE
+    NOREPLICATION;
---
+++ src/main/data/generated/version/0.0.0/001-role/004-AafCoreAdministrator.sql
@@ -0,0 +1,11 @@
+-- Role: "AafCoreAdministrator"
+
+-- DROP ROLE "AafCoreAdministrator";
+
+CREATE ROLE "AafCoreAdministrator" WITH
+    LOGIN
+    NOSUPERUSER
+    NOINHERIT
+    NOCREATEDB
+    NOCREATEROLE
+    NOREPLICATION;
