8ad18686e1037ff5ec463e0aa394623aa83cd7a2
Doug Wilson <doug@revealix.com>
2024-06-20 17:31:20 +0200

    Add WHERE filter, ORDER BY, and LIMIT & OFFSET to Postgres function

--- entitydatamicroservice/src/main/java/com/dsimpletech/aafdata/EntityDataMicroservice/controller/BusinessEntityController.java
+++ entitydatamicroservice/src/main/java/com/dsimpletech/aafdata/EntityDataMicroservice/controller/BusinessEntityController.java
@@ -9,9 +9,9 @@ import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 
-//import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 
-//import org.springframework.util.MultiValueMap;
+import org.springframework.util.MultiValueMap;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -25,12 +25,17 @@ import org.springframework.web.bind.annotation.ResponseBody;
 
 import org.springframework.web.server.ServerWebExchange;
 
+import java.lang.Exception;
+
 import java.sql.CallableStatement;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Types;
+import java.time.LocalDateTime;
 
 import com.dsimpletech.aafdata.EntityDataMicroservice.database.DatabaseConnection;
+
+import static java.lang.Long.parseLong;
 
 
 @RestController
@@ -68,22 +73,27 @@ public class BusinessEntityController
 
     @GetMapping("/{entityTypeName}")
     @ResponseBody
-    public ResponseEntity<String> GetBusinessEntities(@PathVariable String entityTypeName, ServerWebExchange exchange)
+    public ResponseEntity<String> GetBusinessEntities(@PathVariable String entityTypeName, ServerWebExchange exchange) throws Exception
     {
 
         Connection connection = null;
         CallableStatement statement = null;
-        //MultiValueMap<String,String> queryParams = null;
-        //ServerHttpRequest request = null;
+        MultiValueMap<String,String> queryParams = null;
+        ServerHttpRequest request = null;
         String entityData = "";
         //String result = "";
-        String[] filterIssueValues = null;
+        String[] sqlValues = null;
         String errorValues = "";
         String warnValues = "";
+        LocalDateTime asOfDateTimeUtc = null;
+        long graphDepthLimit = -1;
+        long pageNumber = -1;
+        long pageSize = -1;
 
         //TODO: In Postgres function, return total matching records, e.g. SELECT COUNT(Id)
-
-        //TODO: Remove direct table access with SQL GRANTs; CRUD only enforced upstream through process service calls
+        //TODO: In Postgres function, if possible change ALTER FUNCTION public."EntityDataRead"(character varying) OWNER TO postgres to GRANT
+        //TODO: Index Ordinal???
+        //TODO: Add ReadWrite and ReadOnly roles, and remove direct table access with SQL GRANTs; CRUD only enforced upstream through process service calls
 
         //NOTE: Since we're not automatically parsing the resulting JSON into an object, we're returning a JSON String rather than a JSONObject
 
@@ -96,28 +106,58 @@ public class BusinessEntityController
 
             //TODO: Add version to base URL? Versioning: simple, explicit (v1.2.3) or internal, based on/derived from AsOfUtcDateTime?
 
-            //request = exchange.getRequest();
-            //queryParams = request.getQueryParams();
+            request = exchange.getRequest();
+            queryParams = request.getQueryParams();
+
+            //NOTE: http://localhost:8080/EntityType?where="Id"%3D1&sortBy="Ordinal"%252C"Id"&asOfDateTimeUtc=2023-01-01T00:00:00.000&graphDepthLimit=1&pageNumber=1&pageSize=20
 
             //TODO: Build query parameter array while validating explicit filter criteria and logging invalid attribute names, etc to be returned (see Create GET Method AAF-48)
-                //TODO: Add basic query filters, e.g. =, IN, LIKE, etc, by data type
-                //TODO: Add sort order, e.g. ASC, DESC, including defaults like name, Ordinal, DESC, etc
                 //TODO: Add pagination, e.g. pageNumber, pageSize, with master defaults and max
-                //TODO: Add asOfDateTimeUtc (placeholder for later temporal operations), including NOW() default
-                //TODO: Filter out all deleted
-                //TODO: Filter out all IsActive = false by default
+                //TODO: Filter out all deleted in function
+                //TODO: Filter out all IsActive = false by default in function
 
             //TODO: Use GetBusinessEntities() to cache EntityTypeDefinition, EntityTypeAttribute, and EntityTypeDefinitionEntityTypeAttributeAssociation for input validation at service startup
             //TODO: Validate EntityType name and attributes here rather than in function
 
-            filterIssueValues = environment.getProperty("sqlToErrorAndReport").toLowerCase().split(",");
-            errorValues = GuardAgainstSqlIssues("Id=1".toLowerCase(), filterIssueValues);
+            connection = DatabaseConnection.getConnection();
 
-            filterIssueValues = (environment.getProperty("sqlToWarnAndReport").toLowerCase() + "," + environment.getProperty("sqlToWarnAndReportAggregate").toLowerCase() + "," + environment.getProperty("sqlToWarnAndReportString").toLowerCase()).split(",");
-            warnValues = GuardAgainstSqlIssues("EntityType ASC".toLowerCase(), filterIssueValues);
+            //NOTE: Validate EntityType name
+            statement = connection.prepareCall("{call \"EntityDataRead\"(?,?)}");
+            statement.setString(1, entityTypeName);
 
-            if (errorValues.length() == 0) {
-                connection = DatabaseConnection.getConnection();
+            //NOTE: Register the OUT parameter before calling the stored procedure
+            statement.registerOutParameter(2, Types.LONGVARCHAR);   //TODO: Return total number of entities from function
+            statement.executeUpdate();
+
+            //TODO: Filter or mask unauthorized or sensitive attributes
+
+            //NOTE: Read the OUT parameter now
+            entityData = statement.getString(2);
+
+            if (entityData.indexOf(entityTypeName) < 0)
+            {
+                throw new Exception("EntityType '" + entityTypeName + "' not found in EntityTypeDefinition table, i.e. not a valid EntityType.");
+            }
+
+            //TODO: Check for query parameters before checking them
+            sqlValues = environment.getProperty("sqlNotToAllow").toLowerCase().split(",");
+            errorValues = GuardAgainstSqlIssues(queryParams.toString(), sqlValues);
+
+            //TODO: If no asOfDateTimeUtc, grab ISO string value of Now() in database server's time zone
+            //asOfDateTimeUtc = LocalDateTime.parse(String.valueOf(queryParams.get("asOfDateTimeUtc").get(0)));
+
+            //TODO: If no graphDepthLimit, use default
+            graphDepthLimit = parseLong(String.valueOf(queryParams.get("graphDepthLimit").get(0)), 10);
+
+            //TODO: If no pageNumber, use default
+            pageNumber = parseLong(String.valueOf(queryParams.get("pageNumber").get(0)), 10);
+
+            //TODO: If no pageSize, use default
+            pageSize = parseLong(String.valueOf(queryParams.get("pageSize").get(0)), 10);
+
+            if (errorValues.length() == 0)
+            {
+                //connection = DatabaseConnection.getConnection();
                 statement = connection.prepareCall("{call \"EntityDataRead\"(?,?)}");
                 statement.setString(1, entityTypeName);
 
@@ -128,6 +168,7 @@ public class BusinessEntityController
                 //TODO: Filter or mask unauthorized or sensitive attributes
 
                 //NOTE: Read the OUT parameter now
+                //TODO: Check for null entityData
                 entityData = statement.getString(2);
             }
         }
@@ -161,21 +202,18 @@ public class BusinessEntityController
         return new ResponseEntity<String>(entityData, HttpStatus.OK);
     }
 
-    public String GuardAgainstSqlIssues(String sql, String[] filterIssueValues)
+    public String GuardAgainstSqlIssues(String sqlFragment, String[] sqlBlacklistValues)
     {
         String issueValues = "";
-        //String[] filterIssueValues = null;
 
         try
         {
             //NOTE: Based on Spring Tips: Configuration (https://spring.io/blog/2020/04/23/spring-tips-configuration)
-            //filterIssueValues = new String[]{environment.getProperty("sqlToErrorAndReport").toLowerCase()};
+            //filterIssueValues = new String[]{environment.getProperty("sqlToAllowInWhereClause").toLowerCase()};
 
-            for (int i = 0; i < filterIssueValues.length; i++)
-            {
-                if (sql.contains(filterIssueValues[i]))
-                {
-                    issueValues = issueValues + filterIssueValues[i] + ",";
+             for (String sqlBlacklistValue : sqlBlacklistValues) {
+                if (sqlFragment.contains(sqlBlacklistValue.toLowerCase())) {
+                    issueValues = issueValues + sqlBlacklistValue + ",";
                 }
             }
         }
@@ -185,7 +223,7 @@ public class BusinessEntityController
             return issueValues;
         }
 
-        logger.info("GuardAgainstSqlIssues succeeded when " + sql + " contained " + issueValues);
+        logger.info("GuardAgainstSqlIssues succeeded when " + sqlFragment + " did not contain " + sqlBlacklistValues.toString());
         return issueValues;
     }
 }
--- src/main/data/generated/version/0.0.0/005-function/005-EntityDataRead.sql
+++ src/main/data/generated/version/0.0.0/005-function/005-EntityDataRead.sql
@@ -1,23 +1,28 @@
--- FUNCTION: EntityDataRead
+-- FUNCTION: public.EntityDataRead(character varying)
 
--- DROP FUNCTION "EntityDataRead";
+-- DROP FUNCTION public."EntityDataRead"(character varying);
 
-CREATE FUNCTION "EntityDataRead"
-(
-    entityName IN varchar(100)
-)
-    RETURNS varchar
-    LANGUAGE plpgsql
-AS
-$$
+CREATE OR REPLACE FUNCTION public."EntityDataRead"(
+	entitytypename character varying)
+    RETURNS character varying
+    LANGUAGE 'plpgsql'
+
+    COST 100
+    VOLATILE 
+    
+AS $BODY$
 DECLARE
     entityData varchar;
-    sqlQuery varchar := 'SELECT json_agg("' || entityName || '") FROM "' || entityName || '"."' || entityName || '"';
+--    sqlQuery varchar := 'SELECT json_agg("' || entitytypename || '" ORDER BY "Id" DESC) FROM "' || entitytypename || '"."' || entitytypename || '" WHERE "Id">0';
+    sqlQuery varchar := 'WITH JsonData AS (SELECT * FROM "EntityType"."EntityType" WHERE "Id">0 ORDER BY "Id" DESC LIMIT 3 OFFSET 0) SELECT JSON_AGG(JsonData.*) FROM JsonData;';
 
-BEGIN
+  BEGIN
 
     EXECUTE sqlQuery INTO entityData;
     RETURN entityData;
 
-END;
-$$
+  END;
+$BODY$;
+
+ALTER FUNCTION public."EntityDataRead"(character varying)
+    OWNER TO postgres;
