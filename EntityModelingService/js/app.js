0b2ed4f7cfe3d838c80a1df21782e574087730d8
Doug Wilson <doug@revealix.com>
2025-05-06 16:55:14 +0200

    Link the EntityTypeDefinition and EntityTypeAttribute Tables
    + When the page is initially loaded, populate both tables using the values specified in the index.html page
    
    + Once the EntityTypeDefinition has been populated, default its selected row to the first row, and use the EntityType's Id value to populate the EntityTypeAttribute table

--- EntityModelingService/js/app.js
+++ EntityModelingService/js/app.js
@@ -1,34 +1,16 @@
 //NOTE: Copyright © 2003-2025 Deceptively Simple Technologies Inc. Some rights reserved. Please see the aafdata/LICENSE.txt file for details.
 
-console.log(`app.js executing! No DOMContentLoaded listener added yet.`);
-
-//NOTE: Constructors for both EntityTables defined in index.html are called here
-
 //NOTE: "Wire up" the two EntityTables so that when a row is clicked in the first EntityTable, the data is sent to the second EntityTable
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('entityTablePopulated', handleEntityTablePopulated);
+
+function handleEntityTablePopulated(event) {
   const entityTableDefinitions = document.getElementById('entitytypedefinition');
   const entityTableAttributes = document.getElementById('entitytypeattribute');
 
-  console.log(`app.js still executing! DOMContentLoaded listener now added to page.`);
-
-  //NOTE: Populate the definitions table with data by adding custom listener and dispatching custom event
-  const definitionsEvent = new CustomEvent('entityTableCreated', {
-    detail: { entityTableDefinitions }
-  });
-
-  console.log(`app.js still executing! Dispatching entityTableCreated event ...`);
-  entityTableDefinitions.dispatchEvent(definitionsEvent);
-
-  const selectedRow = entityTableDefinitions.getElementsByTagName('tr')[0]; //NOTE: Get the first row in the table
-
-  //NOTE: Populate the attributes table with data by adding custom listener and dispatching custom event
-  const attributesEvent = new CustomEvent('entityTableRowClicked', {
-    detail: { selectedRow }
-  });
-
-  console.log(`app.js still executing! Dispatching entityTableRowClicked event ...`);
-  entityTableAttributes.dispatchEvent(attributesEvent);
-
-  //TODO: Add click event listener to each row in the first table??? (unless passing the EntityTable reference is sufficient)
-
-});
+  if (event.detail.tableId === 'entitytypedefinition') {
+    //NOTE: Get the first data row in the table, i.e. not the header row
+    const selectedRow = entityTableDefinitions.shadowRoot.querySelectorAll('tbody tr')[0];
+    console.log(`Selected row id: ${selectedRow.querySelectorAll('td')[0].innerText}`);
+    entityTableAttributes.refreshData(entityTableAttributes.getAttribute('baseUrl') || 'http://localhost:8080/entityTypeAttributes/', entityTableAttributes.getAttribute('entityTypeName') || 'EntityTypeAttribute', '%22EntitySubtypeId%22%20%3D%2067', entityTableAttributes.getAttribute('sortClause') || '%22Ordinal%22%20ASC', entityTableAttributes.getAttribute('pageSize') || 20, entityTableAttributes.getAttribute('pageNumber') || 1)
+  }
+}
--- EntityModelingService/js/webcomponent/EntityTable.js
+++ EntityModelingService/js/webcomponent/EntityTable.js
@@ -1,15 +1,14 @@
 //NOTE: Copyright © 2003-2025 Deceptively Simple Technologies Inc. Some rights reserved. Please see the aafdata/LICENSE.txt file for details.
 
 class EntityTable extends HTMLElement {
+
   constructor() {
     super();
-    const shadow = this.attachShadow({ mode: 'open' });
-
-    console.log(`EntityTable constructor ends.`);
+    this.attachShadow({ mode: 'open' });
   }
 
   //NOTE: Called each time this custom element is added to the document, and the specification recommends that custom element setup be performed in this callback rather than in the constructor
-  connectedCallback() {
+  async connectedCallback() {
     const link = document.createElement('link');
     link.setAttribute('rel', 'stylesheet');
     link.setAttribute('href', 'css/style.css');
@@ -18,94 +17,178 @@ class EntityTable extends HTMLElement {
     //TODO: Get live authentication token
     document.cookie = "Authentication=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCIsImtpZCI6ImxvYyJ9.eyJpc3MiOiJBQUZEYXRhLUNsaWVudCIsInN1YiI6IkF1dGhlbnRpY2F0aW9uIiwiYXVkIjoiQUFGRGF0YS1FbnRpdHlEYXRhTWljcm9zZXJ2aWNlIiwiZXhwIjoxNzIzODE2OTIwLCJpYXQiOjE3MjM4MTY4MDAsIm5iZiI6MTcyMzgxNjc4OSwianRpIjoiZWY0YWY0ZTMtZTczNi00MjVhLWFhZmYtZWNhMDNiN2I5YjI4IiwiYm9keSI6eyJFbWFpbEFkZHJlc3MiOiJhbXkuYW5kZXJzb25AYW15c2FjY291bnRpbmcuY29tIn19.a5UdLPgo4CzMyK1_JuWbQQQEMYz-rBcLu5uH0sZElqw; path=/";
 
-    const table = document.createElement('table');
+    // const table = document.createElement('table');
+    //
+    // // console.log(`EntityTable connectedCallback() Explicit parent id: ${this.getAttribute('id')}`);
+    // table.setAttribute('id', this.getAttribute('id') || 'entitytypedefinition-' + Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000);
+    // table.setAttribute('class', 'entity-table');
+    //
+    try {
+      await this.refreshData(this.getAttribute('baseUrl') || 'http://localhost:8080/entityTypes/', this.getAttribute('entityTypeName') || 'EntityTypeDefinition', this.getAttribute('whereClause') || '%22Id%22%20%3E%20-2', this.getAttribute('sortClause') || '%22Ordinal%22%20ASC', this.getAttribute('pageSize') || 20, this.getAttribute('pageNumber') || 1);
+    //   const data = await this.fetchData(this.getAttribute('baseUrl') || 'http://localhost:8080/entityTypes/', this.getAttribute('entityTypeName') || 'EntityTypeDefinition', this.getAttribute('whereClause') || '%22Id%22%20%3E%20-2', this.getAttribute('sortClause') || '%22Ordinal%22%20ASC', this.getAttribute('pageSize') || 20, this.getAttribute('pageNumber') || 1);
+    //   //TODO: Check if data is empty and handle it accordingly
+    //   this.displayData(data, table, this.getAttribute('includeColumns') || ['Id', 'EntitySubtypeId', 'TextKey'], this.getAttribute('zeroWidthColumns') || []);
+    }
+    catch (error) {
+    //   console.error('Error fetching data:', error);
+    //   //TODO: Display error message in the table
+    }
+    //
+    // this.shadowRoot.appendChild(table);
+    //
+    const definitionsEvent = new CustomEvent('entityTablePopulated', {
+      detail: {
+        tableId: this.getAttribute('id')
+      },
+    });
 
-    console.log(`EntityTable connectedCallback() Explicit parent id: ${this.getAttribute('id')}`);
-    table.setAttribute('id', this.getAttribute('id') + '-23456' || 'entitytypedefinition-34567')   //TODO: Generate and append a unique id
+    document.dispatchEvent(definitionsEvent);
+  }
+
+  async refreshData(baseUrl, entityTypeName, whereClause, sortClause, pageSize, pageNumber) {
+    let table = this.shadowRoot.querySelector('table');
+
+    if (table) {
+      table.remove();
+    }
+
+    table = document.createElement('table');
+
+    // console.log(`EntityTable connectedCallback() Explicit parent id: ${this.getAttribute('id')}`);
+    table.setAttribute('id', this.getAttribute('id') || 'entitytypedefinition-' + Math.floor(Math.random() * (99999 - 10000 + 1)) + 10000);
     table.setAttribute('class', 'entity-table');
 
-    console.log(`EntityTable connectedCallback() Not fetching data yet!`);
+    try {
+      // const data = await this.fetchData(this.getAttribute('baseUrl') || 'http://localhost:8080/entityTypes/', this.getAttribute('entityTypeName') || 'EntityTypeDefinition', this.getAttribute('whereClause') || '%22Id%22%20%3E%20-2', this.getAttribute('sortClause') || '%22Ordinal%22%20ASC', this.getAttribute('pageSize') || 20, this.getAttribute('pageNumber') || 1);
+      const data = await this.fetchData(baseUrl, entityTypeName, whereClause, sortClause, pageSize, pageNumber);
+      //TODO: Check if data is empty and handle it accordingly
+      this.displayData(data, table, this.getAttribute('includeColumns') || ['Id', 'EntitySubtypeId', 'TextKey'], this.getAttribute('zeroWidthColumns') || []);
+    }
+    catch (error) {
+      console.error('Error fetching data:', error);
+      //TODO: Display error message in the table
+    }
 
-    //TODO: Should this be put back into the constructor???
-    //TODO: And should the event listener be added to the document or to the table???
-    this.addEventListener(this.getAttribute('eventListener') || 'DOMContentLoaded', async () => {
-      console.log('EntityTable connectedCallback() ' + this.getAttribute('eventListener') + ` event listener added.`);
-      try {
-        if ((this.getAttribute('eventListener') == 'entityTableCreated') || (this.getAttribute('eventListener') == 'entityTableRowClicked')) {
-          const data = await fetchData(this.getAttribute('baseUrl') || 'http://localhost:8080/entityTypes/', this.getAttribute('entityTypeName') || 'EntityTypeDefinition', this.getAttribute('whereClause') || '%22Id%22%20%3E%20-2', this.getAttribute('sortClause') || '%22Ordinal%22%20ASC', this.getAttribute('pageSize') || 20, this.getAttribute('pageNumber') || 1);
-          displayData(data, table, this.getAttribute('includeColumns') || ['Id', 'EntitySubtypeId', 'TextKey'], this.getAttribute('zeroWidthColumns') || []);
+    this.shadowRoot.appendChild(table);
+    //
+    // this.connectedCallback();
+  }
+
+  async fetchData(baseUrl, entityTypeName, whereClause, sortClause, pageSize, pageNumber) {
+    const response = await fetch(baseUrl + entityTypeName + '?whereClause=' + whereClause + '&sortClause=' + sortClause + '&pageNumber=' + pageNumber + '&pageSize=' + pageSize, {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'http://localhost:8082',
+        'Access-Control-Allow-Credentials': 'true',
+        'ApiKey': '6fdcf7a4-6442-4c1e-a375-9f8064ea34d0',
+        'CorrelationUuid': '83b0361c-108f-4fe6-b95b-fc6f6d645e76'
+      },
+      credentials: 'include' //NOTE: This will include cookies with the request
+    });
+
+    const responseJson = await response.json();
+    return responseJson;
+  }
+
+  displayData(data, table, includeColumns, zeroWidthColumns) {
+    const caption = data.EntityType;
+    table.createCaption().textContent = caption;
+
+    const firstItem = data.EntityData[0];
+    const headerRow = table.createTHead().insertRow();
+    Object.keys(firstItem).forEach(attributeName => {
+      if (includeColumns.includes(attributeName)) {
+        const headerCell = headerRow.insertCell();
+        headerCell.textContent = attributeName;
+        headerCell.width = '33%';
+
+        if (zeroWidthColumns.includes(attributeName)) {
+          headerCell.style.display = 'none';
         }
-      }
-      catch (error) {
-        console.error('Error fetching data:', error);
       }
     });
 
-    this.shadowRoot.appendChild(table);
+    const body = table.createTBody()
+
+    data.EntityData.forEach(item => {
+      const bodyRow = body.insertRow();
+      Object.entries(item).forEach(([key, value]) => {
+        if (includeColumns.includes(key)) {
+          const bodyCell = bodyRow.insertCell();
+          bodyCell.textContent = value;
+          bodyCell.width = '33%';
+
+          if (zeroWidthColumns.includes(key)) {
+            bodyCell.style.display = 'none';
+          }
+        }
+      });
+    });
+
+    const footerRow = table.createTFoot().insertRow();
+    const footerCell = footerRow.insertCell();
+    footerCell.colSpan = Object.keys(firstItem).length;
+    footerCell.textContent = `Records: ${data.TotalRows}`;
   }
 }
 
 customElements.define('entity-table', EntityTable)
 
-async function fetchData(baseUrl, entityTypeName, whereClause, sortClause, pageSize, pageNumber) {
-  const response = await fetch(baseUrl + entityTypeName + '?whereClause=' + whereClause + '&sortClause=' + sortClause + '&pageNumber=' + pageNumber + '&pageSize=' + pageSize, {
-    method: 'GET',
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': 'http://localhost:8082',
-      'Access-Control-Allow-Credentials': 'true',
-      'ApiKey': '6fdcf7a4-6442-4c1e-a375-9f8064ea34d0',
-      'CorrelationUuid': '83b0361c-108f-4fe6-b95b-fc6f6d645e76'
-    },
-    credentials: 'include' //NOTE: This will include cookies with the request
-  });
-
-  if (!response.ok) {
-    throw new Error('Network response was not ok');
-  }
-
-  const responseText = await response.text();
-  data = JSON.parse(responseText);
-  return data;
-}
-
-function displayData(data, table, includeColumns, zeroWidthColumns) {
-  const caption = data.EntityType;
-  table.createCaption().textContent = caption;
-
-  const firstItem = data.EntityData[0];
-  const headerRow = table.createTHead().insertRow();
-  Object.keys(firstItem).forEach(attributeName => {
-    if (includeColumns.includes(attributeName)) {
-      const headerCell = headerRow.insertCell();
-      headerCell.textContent = attributeName;
-      headerCell.width = '33%';
-
-      if (zeroWidthColumns.includes(attributeName)) {
-        headerCell.style.display = 'none';
-      }
-    }
-  });
-
-  const body = table.createTBody()
-
-  data.EntityData.forEach(item => {
-    const bodyRow = body.insertRow();
-    Object.entries(item).forEach(([key, value]) => {
-      if (includeColumns.includes(key)) {
-        const bodyCell = bodyRow.insertCell();
-        bodyCell.textContent = value;
-        bodyCell.width = '33%';
-
-        if (zeroWidthColumns.includes(key)) {
-          bodyCell.style.display = 'none';
-        }
-      }
-    });
-  });
-
-  const footerRow = table.createTFoot().insertRow();
-  const footerCell = footerRow.insertCell();
-  footerCell.colSpan = Object.keys(firstItem).length;
-  footerCell.textContent = `Records: ${data.TotalRows}`;
-}
+// async function fetchData(baseUrl, entityTypeName, whereClause, sortClause, pageSize, pageNumber) {
+//   const response = await fetch(baseUrl + entityTypeName + '?whereClause=' + whereClause + '&sortClause=' + sortClause + '&pageNumber=' + pageNumber + '&pageSize=' + pageSize, {
+//     method: 'GET',
+//     headers: {
+//       'Content-Type': 'application/json',
+//       'Access-Control-Allow-Origin': 'http://localhost:8082',
+//       'Access-Control-Allow-Credentials': 'true',
+//       'ApiKey': '6fdcf7a4-6442-4c1e-a375-9f8064ea34d0',
+//       'CorrelationUuid': '83b0361c-108f-4fe6-b95b-fc6f6d645e76'
+//     },
+//     credentials: 'include' //NOTE: This will include cookies with the request
+//   });
+//
+//   const responseJson = await response.json();
+//   return responseJson;
+// }
+//
+// function displayData(data, table, includeColumns, zeroWidthColumns) {
+//   const caption = data.EntityType;
+//   table.createCaption().textContent = caption;
+//
+//   const firstItem = data.EntityData[0];
+//   const headerRow = table.createTHead().insertRow();
+//   Object.keys(firstItem).forEach(attributeName => {
+//     if (includeColumns.includes(attributeName)) {
+//       const headerCell = headerRow.insertCell();
+//       headerCell.textContent = attributeName;
+//       headerCell.width = '33%';
+//
+//       if (zeroWidthColumns.includes(attributeName)) {
+//         headerCell.style.display = 'none';
+//       }
+//     }
+//   });
+//
+//   const body = table.createTBody()
+//
+//   data.EntityData.forEach(item => {
+//     const bodyRow = body.insertRow();
+//     Object.entries(item).forEach(([key, value]) => {
+//       if (includeColumns.includes(key)) {
+//         const bodyCell = bodyRow.insertCell();
+//         bodyCell.textContent = value;
+//         bodyCell.width = '33%';
+//
+//         if (zeroWidthColumns.includes(key)) {
+//           bodyCell.style.display = 'none';
+//         }
+//       }
+//     });
+//   });
+//
+//   const footerRow = table.createTFoot().insertRow();
+//   const footerCell = footerRow.insertCell();
+//   footerCell.colSpan = Object.keys(firstItem).length;
+//   footerCell.textContent = `Records: ${data.TotalRows}`;
+// }
